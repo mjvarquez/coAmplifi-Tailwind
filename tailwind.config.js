0b5a458a3c8e4fd25b4ad1dfb6be58237a0bdfef
janrey <janreycablinda@gmail.com>
2022-11-12 12:21:59 +0800

    revert to indigo

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,8 @@ const themes = {
     // Default theme is required for theming system to work correctly
     'default': {
         primary  : {
-            ...colors.orange,
-            DEFAULT: colors.black[500]
+            ...colors.indigo,
+            DEFAULT: colors.indigo[600]
         },
         accent   : {
             ...colors.slate,
