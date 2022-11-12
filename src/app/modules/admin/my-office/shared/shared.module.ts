79b38c10dc062311286cdb6b6488f142f6fbe197
janrey <janreycablinda@gmail.com>
2022-11-12 16:10:06 +0800

    3rd commit

--- src/app/modules/admin/my-office/shared/shared.module.ts
+++ src/app/modules/admin/my-office/shared/shared.module.ts
@@ -3,16 +3,32 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { SubMenuComponent } from './sub-menu/sub-menu.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatTabsModule} from '@angular/material/tabs';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
     declarations: [
         BreadcrumbComponent,
         SubMenuComponent
     ],
-    imports: [],
+    imports: [
+        MatIconModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatTabsModule,
+        RouterModule
+    ],
     exports: [
         BreadcrumbComponent,
-        SubMenuComponent
+        SubMenuComponent,
+        MatIconModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatTabsModule
     ]
 })
 export class ClipboardSharedModule
--- src/app/modules/admin/my-office/shared/sub-menu/sub-menu.component.ts
+++ src/app/modules/admin/my-office/shared/sub-menu/sub-menu.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-sub-menu',
   templateUrl: './sub-menu.component.html',
-  styleUrls: ['./sub-menu.component.scss']
+  styleUrls: ['./sub-menu.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SubMenuComponent implements OnInit {
 
