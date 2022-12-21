fd44ec395b36e3f681f66e58b617e022215faabd
MarVills <villaflormarbenc@gmail.com>
2022-12-21 09:23:14 +0800

    working on settings component

--- src/app/modules/admin/my-console/settings/settings.module.ts
+++ src/app/modules/admin/my-console/settings/settings.module.ts
@@ -1,37 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // import { MeetingsComponent } from './meetings.component';
-import { Route } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
 
 import { FuseCardModule } from '@fuse/components/card';
-import { SettingsComponent } from 'app/layout/common/settings/settings.component';
+import { SettingsComponent } from './settings.component';
 
 
-const MeetingsRoutes: Route[] = [
-//   {
-//       path     : '',
-//       title: 'My Meetings',
-//       component: MeetingsComponent,
-//       data: {
-//         sub_menu: [
-//             {
-//                 label: 'Overview', 
-//                 icon: 'mat_outline:groups'
-//             },
-//             {
-//                 label: 'Meetings', 
-//                 icon: 'mat_outline:list'
-//             }
-//         ]
-//     },
-//   }
+
+const SettingRoutes: Route[] = [
+  {
+      path     : '',
+      title: 'Settings',
+      component: SettingsComponent,
+    //   data: {
+    //     sub_menu: [
+    //         {
+    //             label: 'Overview', 
+    //             icon: 'mat_outline:groups'
+    //         },
+    //         {
+    //             label: 'Meetings', 
+    //             icon: 'mat_outline:list'
+    //         }
+    //     ]
+    // },
+  }
 ];
 
 @NgModule({
   declarations: [
    SettingsComponent
   ],
-  imports: [],
+  imports: [
+    RouterModule.forChild(SettingRoutes),
+  ],
   exports: []
 })
 
